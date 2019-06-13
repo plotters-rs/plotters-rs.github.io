724e44e29a16db6836857b1195277033105f9d3b
Hao Hou <haohou302@gmail.com>
2019-06-13 17:00:15 -0600

    Add WASM32 docs

---
+++ wasm32/implementors/core/convert/trait.Into.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["plotters"] = [{text:"impl&lt;'a, 'b: 'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;<a class=\"struct\" href=\"plotters/style/struct.TextStyle.html\" title=\"struct plotters::style::TextStyle\">TextStyle</a>&lt;'a&gt;&gt; for &amp;'b <a class=\"struct\" href=\"plotters/style/struct.TextStyle.html\" title=\"struct plotters::style::TextStyle\">TextStyle</a>&lt;'a&gt;",synthetic:false,types:["plotters::style::TextStyle"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ wasm32/plotters/drawing/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["DrawingAreaErrorKind","The error description of any drawing area API"]],"mod":[["backend",""]],"struct":[["CanvasBackend","The backend that is drawing on the HTML canvas TODO: Support double bufferring"],["DrawingArea","The abstraction of a region"]],"trait":[["IntoDrawingArea","A type which can be converted into a root drawing area"]]});
