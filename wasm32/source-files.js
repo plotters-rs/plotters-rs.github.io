8460a5737225a39ef21ee84f420276c818f04959
Hao Hou <haohou302@gmail.com>
2019-06-13 14:29:00 -0600

    Add WASM32 doc

---
+++ wasm32/source-files.js
@@ -0,0 +1,14 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["bumpalo"] = {"name":"","dirs":[{"name":"collections","dirs":[{"name":"str","dirs":[],"files":["lossy.rs","mod.rs"]}],"files":["mod.rs","raw_vec.rs","string.rs","vec.rs"]}],"files":["alloc.rs","lib.rs"]};
+sourcesIndex["cfg_if"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["lazy_static"] = {"name":"","dirs":[],"files":["inline_lazy.rs","lib.rs"]};
+sourcesIndex["log"] = {"name":"","dirs":[],"files":["lib.rs","macros.rs"]};
+sourcesIndex["proc_macro2"] = {"name":"","dirs":[],"files":["fallback.rs","lib.rs","strnom.rs","wrapper.rs"]};
+sourcesIndex["quote"] = {"name":"","dirs":[],"files":["ext.rs","lib.rs","runtime.rs","to_tokens.rs"]};
+sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","dirs":[],"files":["gen_helper.rs","visit.rs"]}],"files":["attr.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","error.rs","export.rs","expr.rs","ext.rs","file.rs","generics.rs","group.rs","ident.rs","item.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","tt.rs","ty.rs"]};
+sourcesIndex["unicode_xid"] = {"name":"","dirs":[],"files":["lib.rs","tables.rs"]};
+sourcesIndex["wasm_bindgen_backend"] = {"name":"","dirs":[],"files":["ast.rs","codegen.rs","defined.rs","encode.rs","error.rs","lib.rs","util.rs"]};
+sourcesIndex["wasm_bindgen_macro"] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex["wasm_bindgen_macro_support"] = {"name":"","dirs":[],"files":["lib.rs","parser.rs"]};
+sourcesIndex["wasm_bindgen_shared"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
