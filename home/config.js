a4f9ef68e64b92a25c803ef59f3836d092cd5dfc
Hao Hou <haohou302@gmail.com>
2019-09-30 15:16:17 -0600

    Update config

--- home/config.js
+++ home/config.js
@@ -1,7 +1,7 @@
 noddityConfig = {
 	title: 'Plotters - A drawing library for data visualization in pure Rust',
 	noddityRoot: window.location.pathname + 'content/',
-	editLink: 'https://github.com/plotters-rs/plotters-rs.github.io/tree/master/home/',
+	editLink: 'https://github.com/plotters-rs/plotters-rs.github.io/tree/master/home/content/',
 	logo: 'logo.png',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
