e7d71c92a01c61175ccc9aab0d8272fed1277448
Hao Hou <haohou302@gmail.com>
2019-09-30 15:10:45 -0600

    Add new logo

--- home/config.js
+++ home/config.js
@@ -2,7 +2,7 @@ noddityConfig = {
 	title: 'Plotters - A drawing library for data visualization in pure Rust',
 	noddityRoot: window.location.pathname + 'content/',
 	editLink: 'https://github.com/plotters-rs/plotters-rs.github.io/tree/master/home',
-	logo: 'logo.svg',
+	logo: 'logo.png',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
 	pagePathPrefix: 'post/',
