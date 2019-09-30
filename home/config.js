72327c9450c5e7d7d0f5bd4117a8bc8345c612a2
Hao Hou <haohou302@gmail.com>
2019-09-30 14:43:11 -0600

    Change configure

--- home/config.js
+++ home/config.js
@@ -1,7 +1,7 @@
 noddityConfig = {
-	title: 'Noddity',
+	title: 'Plotters - A drawing library for data visualization in pure Rust',
 	noddityRoot: window.location.pathname + 'content/',
-	editLink: 'https://github.com/TehShrike/noddity/edit/gh-pages/content/',
+	editLink: 'https://github.com/plotters-rs/plotters-rs.github.io/tree/master/home',
 	logo: 'logo.svg',
 	errorPage: '404.md',
 	pathPrefix: '#!/',
