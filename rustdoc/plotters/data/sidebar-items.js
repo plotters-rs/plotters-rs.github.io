3fb61c9d73248dc0a6ed84552d92c9393d090dc9
Hao Hou <haohou302@gmail.com>
2019-11-26 11:46:20 -0700

    Add new documentation and sync the data repo

--- rustdoc/plotters/data/sidebar-items.js
+++ rustdoc/plotters/data/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["fitting_range","Build a range that fits the data"]]});
+initSidebarItems({"fn":[["fitting_range","Build a range that fits the data"]],"struct":[["Quartiles","The quartiles"]]});
